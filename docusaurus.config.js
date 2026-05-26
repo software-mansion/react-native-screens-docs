849fbc1a0dce27ae72f8a832873238a6054dbbc5
Konrad Michalik <konrad.michalik@swmansion.com>
2026-05-26 14:02:53 +0200

    chore: Prepare repo for develop - update t-rex-ui, colors, formatting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
           },
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve('./src/css/index.css'),
         },
       }),
     ],
